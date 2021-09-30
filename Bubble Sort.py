1f1f4f31f5de550da32536c758879b1403b3298e
Pratyush Arora <arora.pratyush01@gmail.com>
2021-09-30 23:22:19 +0530

    updated the bubble sort

--- Bubble Sort.py
+++ Bubble Sort.py
@@ -1,7 +1,7 @@
 #Bubble Sort
-a=[int(x) for x in input().split()]
+a=[int(x) for x in input().strip().split()]
 for x in range(0,len(a)-1):
     for y in range(0,len(a)-1-x):
         if a[y]>a[y+1]:
             a[y],a[y+1]=a[y+1],a[y]
-print(a)
+print(*a)
