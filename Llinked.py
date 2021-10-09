cc99f826cecbaec92fd0418993a9d331c074a47a
Pratyush Arora <arora.pratyush01@gmail.com>
2021-10-09 20:28:43 +0530

    updated my linked list

--- Llinked.py
+++ Llinked.py
@@ -2,14 +2,18 @@ class node:
     def __init__(self,data):
         self.data=data
         self.next=None
+
 class LinkedList:
     def __init__(self):
         self.head=None
+
 
     def add(self,data):
         new_node=node(data)
         new_node.next=self.head
         self.head=new_node
+
+
     def push(self,data):
         new_node=node(data)
         if self.head == None:
@@ -20,6 +24,8 @@ class LinkedList:
                 tmp=tmp.next
             tmp.next=new_node
             new_node.next=None
+
+
     def place(self,data,index=0):
         tmp=self.head
         new_node=node(data)
@@ -40,6 +46,27 @@ class LinkedList:
         tmp.next=new_node
         new_node.next=tmp2
 
+    def delete(self,pos=0):
+        tmp=self.head
+        if(self.head==None):
+            print("List is empty, postion not found")
+            return
+        if(pos==0):
+            self.head=tmp.next
+            return
+        for x in range(pos-1):
+            if(tmp.next==None):
+                print("KeyError Position greater then list")
+                return
+            tmp=tmp.next
+        if (tmp==None or tmp.next ==None):
+            print("KeyError Position greater then list")
+            return
+        tmp2=tmp.next.next
+        tmp.next = None
+        tmp.next=tmp2
+
+
     def printList(self):
         tmp=self.head
         while tmp != None:
@@ -47,18 +74,31 @@ class LinkedList:
             if(tmp.next!=None):
                 print("->",end=" ")
             tmp=tmp.next
+        print()
 
-def generator():
-    llist = LinkedList()
-    llist.add(7)
-    llist.add(1)
-    llist.add(3)
-    llist.add(2)
-    llist.push(9)
-    llist.place(12,6)
+
+def generator(ar:str,N:int,llist):
+    elem=[int(x) for x in ar.strip().split()]
+    for x in range(N):
+        llist.add(elem[x])
     llist.printList()
+def deletion(llist):
+    pos=int(input("enter index of number to delete\n"))
+    llist.delete(pos)
+    llist.printList()
+
+    
 def main():
-    generator()
+    llist=LinkedList()
+    len_ar=int(input("enter the number of elements=>  "))
+    ar_str=str(input("enter {} space separated integers=> ".format(len_ar)))
+    generator(ar_str,len_ar,llist)
+    token=input("do you want to delete a element  ")
+        if (token=="y"):
+            deletion(llist)
+        else:
+            print("Bbye")    
 #driver code
 main()
+ 
 
