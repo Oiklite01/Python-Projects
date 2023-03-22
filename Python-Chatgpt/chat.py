46291152483ea2f577c1db91fef632d0bf5c3486
Pratyush Arora <arora.pratyush01@gmail.com>
2023-03-22 10:28:35 +0530

    Revert "pandas chatgpt"
    This reverts commit 67191f811438b047b4f51f7283af958b0835025b.

--- Python-Chatgpt/chat.py
+++ Python-Chatgpt/chat.py
@@ -1,42 +1,19 @@
 import random
 import openai
-class ai:
-    def __init__(self):
-        self.x=0
-        self.ans=""
+openai.api_key="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"
+eng = "text-davinci-003"
+ques="There is no good sweet store available near my house"
+a = round(random.uniform(0.1,0.9),2)
+completion=openai.Completion.create(
+    engine=eng,
+    prompt=ques,
+    max_tokens=100,
+    temperature=a,
+    n=1
+)
+response=completion.choices[0].text
+print(response)
 
-    def get(self):
-        print("Enter Number of Responses :")
-        while(True):
-            try:
-                self.x=int(input())
-                break
-            except:
-                print("Invalid Input Try Again")
-        self.talk()
-    def talk(self):
-        print("Hello Lets Start talking")
-        ctr=self.x
-        while(ctr>0):
-            q=str(input())
-            self.ans=self.response(q)
-            print(self.ans)
-            ctr-=1
-        print("Thats all in this session")
 
-        
-    def response(self,q : str)->str:
-        openai.api_key="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"
-        eng = "text-davinci-003"
-        ques=q
-        a = round(random.uniform(0.1,0.3),2)
-        completion=openai.Completion.create(
-            engine=eng,
-            prompt=ques,
-            max_tokens=100,
-            temperature=a,
-            n=1)
-        reply=completion.choices[0].text
-        return reply
-ob1= ai()
-ob1.get()
+
+
