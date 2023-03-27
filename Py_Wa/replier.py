3d031ebbd5dd8d522b7cf4b25ec1b782ba278c0b
Pratyush Arora <arora.pratyush01@gmail.com>
2023-03-27 12:20:29 +0530

    Whatsapp bot made

---
+++ Py_Wa/replier.py
@@ -0,0 +1,53 @@
+import pyautogui as pt
+from time import sleep
+import openai
+import pyperclip
+
+def resp(s:str)->str:
+    import random
+    openai.api_key="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"
+    eng = "text-davinci-003"
+    ques=s
+    a = round(random.uniform(0.1,0.3),2)
+    completion=openai.Completion.create(
+        engine=eng,
+        prompt=ques,
+        max_tokens=100,
+        temperature=a,
+        n=1
+    )
+    response=completion.choices[0].text
+    return response
+
+def getmsg():
+    pt.moveTo(x,y,duration=.1)
+    pt.tripleClick()
+    pt.hotkey('ctrl','c')
+    s=pyperclip.paste()
+    return s
+def send():
+    global x,y
+    sleep(0.5)
+    reply=resp(getmsg())
+    pyperclip.copy(reply)
+    sleep(0.5)
+    pt.moveTo(x+100,y+60,duration=.1)
+    pt.leftClick()
+    sleep(0.5)
+    pt.hotkey('ctrl' , 'v')
+    pt.press('enter')
+#driver code
+pt.hotkey('alt', 'tab')
+x=1
+while(x!=0):
+    sleep(0.1)
+    pos=pt.locateOnScreen("smiley.png")
+    x=pos[0]+75
+    y=pos[1]-43
+    pix = pt.pixel(int(x),int(y))
+    if(pix[0]==32):
+        send()
+    else:
+        pass
+
+
